907f60ebb5e1b44e5794a50c1873c7f74e07c42a
Unai Mengual <unai01@outlook.es>
2018-09-14 03:04:32 +0200

    Fixing error

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 
+    const chalk = require('chalk');
+    const figlet = require('figlet');
+    
     module.exports = (text, color, type, font) => {
         console.log(chalk[color || 'yellow'](
             figlet.textSync(text, {
